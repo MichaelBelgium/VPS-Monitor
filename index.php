5abc116dcce986e4ca48f93359c34b90dcc3751f
Michael V <michaelvrld@gmail.com>
2016-10-28 18:07:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,103 +1,99 @@
 <?php
-	define("DISPLAY", "knob"); //knob or meter
 	define("REFRESH_TIME", 3); //in seconds
-	define("DEBIAN", false); //is this file located on debian or not ?
-	
-	if(DEBIAN)
-		exec(" free | grep \"buffers/cache\" | awk {'print $3+$4\" \"$3\" \"$4'}",$mem);
-	else
-		exec("free | grep \"Mem:\" | awk {'print $2\" \"$3\" \"$4'}",$mem);
-	exec("cat /proc/loadavg | awk {'print $1\" \"$4'}",$cpu);
-	exec("cat /proc/cpuinfo | grep \"model name\"",$cpuinfo);
-	exec("df | grep ".((DEBIAN) ? "rootfs" : "/dev/simfs")." | awk {'print $2\" \"$3\" \"$4'}",$storage);
-	exec("/usr/bin/cut -d. -f1 /proc/uptime",$uptime);
-	exec("cat /proc/net/dev | grep ". ((DEBIAN) ? "eth0" : "venet0") ." | ". ((DEBIAN) ? "awk {'print $2\" \"$3\" \"$10\" \"$11'}": "awk {'print $1\" \"$2\" \"$9\" \"$10'}"),$network);
-	
-	$cpu = explode(" ", $cpu[0]);
-	$processes = explode("/", $cpu[1]);
-	$uptime = $uptime[0];
-	$mem = explode(" ", $mem[0]);
-	$storage = explode(" ", $storage[0]);
-	$network = explode(" ", $network[0]);
-
-	function convertSeconds($ss) 
-	{
-		$s = $ss%60;
-		$m = floor(($ss%3600)/60);
-		$h = floor(($ss%86400)/3600);
-		$d = floor(($ss%2592000)/86400);
-		$M = floor($ss/2592000);
-
-		return "$M months, $d days, $h hours, $m minutes, $s seconds";
-	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>VPS Usage</title>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script src="knob.js"></script>
+		<script src="https://rawgit.com/aterrien/jQuery-Knob/master/js/jquery.knob.js"></script>
 		<style type="text/css">
-			h2 {font-size: 30px; margin-bottom: 70px; margin-top: 0;}
-			span { vertical-align: middle; }
-			label { font-size: 25px; }
-			section { display: inline-block;}
-			div {padding: 10px;}
-			meter { width: 100%; height: 30px;}
+			article { display: inline-block; }
+			article h2 	{ font-size: 30px;	margin: 0;	}
+			article span { display: inline-block;  margin-top: 75px; font-size: 20px;}
+			#content { margin-top: 40px; }
 		</style>
 	</head>
 
 	<body>
-		<div>
-			<p><b>Note:</b>This page refreshes every <?php echo REFRESH_TIME; ?> second(s).</p>
-			<?php if(DISPLAY == "knob"): ?>
-			<section id="mem" data-thickness=".26"><h2>RAM usage</h2><label></label></section>
-			<section id="hdd" data-thickness=".26"><h2>Disk usage</h2><label></label></section>
-			<section id="cpu" data-fgColor="#66CC66" data-thickness=".26"><h2>CPU usage</h2><label></label></section>
-			<p style="margin-top: 50px;">
-			<?php elseif(DISPLAY == "meter"): ?>
-				<label for="mem"></label><meter min="0" max="<?php echo $mem[0]; ?>" low="25" high="75" value="<?php echo $mem[1]; ?>"></meter>
-				<label for="hdd"></label><meter min="0" max="<?php echo $storage[0]; ?>" low="25" high="75" value="<?php echo $storage[1]; ?>"></meter>
-				<label for="cpu"></label><meter min="0" max="100" low="25" high="75" value="<?php echo $cpu[0]; ?>"></meter>
-				<p>
-			<?php
-			endif; 
-				echo "<b>RAM usage</b>:", number_format($mem[1],0,","," ")," kb out of ", number_format($mem[0],0,","," "), " kb used. Free: ",number_format($mem[2],0,","," ")," kb<br>"; 
-				echo "<b>HDD usage</b>: ", number_format($storage[1],0,","," ")," kb out of ", number_format($storage[0],0,","," "), " kb used. Free: ",number_format($storage[2],0,","," "), " kb<br>";
-				echo "<b>Uptime</b>:", convertSeconds($uptime), "<br>";
-				echo "<b>Processes</b>:", $processes[0], " running, ", $processes[1], " sleeping<br>";
-				echo "<b>CPU Info</b>:<ul>"; for ($i=0; $i < count($cpuinfo); $i++) echo "<li>", substr($cpuinfo[$i], 12), "</li>"; echo "</ul>";
-				echo "<b>Network info</b>:<ul><li>Received: ", number_format((DEBIAN) ? $network[0] : substr($network[0], 7),0,","," "), " bytes (", number_format($network[1],0,","," ")," packets)</li><li>Sent: ", number_format($network[2],0,","," ")," bytes (", number_format($network[3],0,","," "), " packets)</li></ul>";
-				echo "</ul></p>";
-			?>
+		<div class="main">
+			<p>
+				<b>Note:</b>This page refreshes every <?php echo REFRESH_TIME; ?> second(s).
+			</p>
+
+			<article id="mem" data-thickness=".26">
+				<h2>RAM usage</h2>
+				<span></span>
+			</article>
+			<article id="hdd" data-thickness=".26">
+				<h2>HDD Usage</h2>
+				<span></span>
+			</article>
+			<article id="cpu" data-fgColor="#66CC66" data-thickness=".26">
+				<h2>CPU Load</h2>
+				<span></span>
+			</article>
+
+			<p id="content"></p>
 		</div>
 		<script type="text/javascript">
-			$("#mem").knob({'readOnly': true, 'max': <?php echo $mem[0]; ?>	});
-			$("#hdd").knob({'readOnly': true, 'max': <?php echo $storage[0]; ?>	});
+		$(document).ready(function() 
+		{
+			setInterval(refresh, <?php echo REFRESH_TIME*1000; ?>);
+			refresh();
+
+			$("#mem").knob();
+			$("#hdd").knob();
 			$("#cpu").knob({'readOnly': true, 'max': 100 });
-			
-			$("#mem").val(<?php echo $mem[1]; ?>).trigger('change');
-			$("#mem label").text(round(<?php echo $mem[1]; ?>/<?php echo $mem[0]; ?>*100) + "%");
-			$("label[for=mem]").text(round(<?php echo $mem[1]; ?>/<?php echo $mem[0]; ?>*100) + "%");
+		});
 
-			$("#hdd").val( <?php echo $storage[1]; ?>).trigger('change');
-			$("#hdd label").text(round( <?php echo $storage[1]; ?>/<?php echo $storage[0]; ?>*100) + "%");
-			$("label[for=hdd]").text(round( <?php echo $storage[1]; ?>/<?php echo $storage[0]; ?>*100) + "%");
+		function refresh()
+		{
+			$.getJSON("getData.php", "", success);
+		}
 
-			var cpuusage = round((<?php echo $cpu[0]; ?>*100) / <?php echo count($cpuinfo); ?>,1);
-			$("#cpu").val(cpuusage).trigger('change');
-			$("#cpu label").text(cpuusage+"%");
-			$("label[for=cpu]").text(cpuusage+"%");
+		function success(data, textStatus, jqXHR)
+		{
+			$("#mem").trigger("configure",{"min":0, "max": data.mem[0] });
+			$("#hdd").trigger("configure",{"min":0, "max": data.storage[0] });
 
-			function round(num,fix) 
-			{ 
-				fix = (typeof fix === 'undefined') ? 3 : fix; 
-				return num.toFixed(fix); 
-			}
+			$("#mem").val(data.mem[1]).trigger("change");
+			$("#mem span").text(((data.mem[1] / data.mem[0]) * 100).toFixed(2) + "%");
 
-			$(document).ready(function() {
-				setTimeout(function(){ location.reload(); }, <?php echo REFRESH_TIME*1000; ?>);
-			});
+			$("#hdd").val(data.storage[1]).trigger("change");
+			$("#hdd span").text( ((data.storage[1] / data.storage[0]) * 100).toFixed(2) + "%" );
+
+			$("#cpu").val(data.cpu_load * 100.0).trigger("change");
+			$("#cpu span").text($("#cpu").val() + "%");
+
+			$("#content").empty();
+			$("#content").html("<b>RAM usage:</b> "+data.mem[1]+" kb out of "+data.mem[0]+" kb used. Free: " + data.mem[2] + "<br />" +
+				"<b>HDD usage:</b> "+data.storage[1]+" out of "+data.storage[0]+" kb used. Free: "+data.storage[2] + "<br />" + 
+				"<b>Uptime:</b> "+ getTime(data.uptime) + "<br />" +
+				"<b>Processes:</b> " + data.cpu_processes[0] + " running, " + data.cpu_processes[1] + " idle <br/>" +
+				"<b>Network stats</b>: <ul><li>Recieved: " + data.network[0] + " bytes (" + data.network[1] + " packets)</li><li>Sent: " + data.network[2] + " bytes (" + data.network[3] + " packets)</li></ul><b>CPU info:</b><p>");
+
+			for (var i = 0; i < data.cpu_info.length; i++) 
+				$("#content").append(data.cpu_info[i] + "<br />");
+
+			$("#content").append("</p>");
+		}
+
+		function getTime(seconds) 
+		{
+		    var leftover = seconds;
+
+		    var days = Math.floor(leftover / 86400);
+		    leftover = leftover - (days * 86400);
+
+		    var hours = Math.floor(leftover / 3600);
+		    leftover = leftover - (hours * 3600);
+
+		    var minutes = Math.floor(leftover / 60);
+		    leftover = leftover - (minutes * 60);
+
+		    return days + " days, " + hours + " hours, " + minutes + " minutes, " + leftover + " seconds";
+		}
 		</script>
 	</body>
 </html>
