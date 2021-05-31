f10db374888a5bc297532eafd5599c3cdc156904
Michael V <michaelvrld@gmail.com>
2021-05-31 11:00:16 +0200

    Indentation to spaces

--- index.php
+++ index.php
@@ -1,82 +1,82 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>VPS Usage</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.7/css/mdb.min.css" />
-	</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>VPS Usage</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.7/css/mdb.min.css" />
+    </head>
 
-	<body>
-		<div class="container-fluid py-2">
-			<div class="row">
-				<div class="col">
-					<div class="alert alert-warning" role="alert"><b>Note:</b> Content refreshes every <span id="refresh_time"></span> second(s).</div>
-					<div class="alert alert-info" role="alert" id="general_info">-</div>
-					<section id="mainchart"></section>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-2">
-					<div class="card" id="ram">
-						<div class="card-header">
-							RAM usage
-						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item list-group-item-danger">Usage: <span class="usage">0</span></li>
-							<li class="list-group-item list-group-item-info">Total: <span class="total">0</span> GB</li>
-							<li class="list-group-item list-group-item-success">Free: <span class="free">0</span> GB</li>
-						</ul>
-						<div class="card-body">
-							<canvas id="doughnut-ram"></canvas>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-2">
-					<div class="card" id="hdd">
-						<div class="card-header">
-							HDD usage
-						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item list-group-item-danger">Usage: <span class="usage">0</span> GB</li>
-							<li class="list-group-item list-group-item-info">Total: <span class="total">0</span> GB</li>
-							<li class="list-group-item list-group-item-success">Free: <span class="free">0</span> GB</li>
-						</ul>
-						<div class="card-body">
-							<canvas id="doughnut-hdd"></canvas>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-2">
-					<div class="card" id="network">
-						<div class="card-header">
-							Network usage
-						</div>
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item">Received: <span class="rec">0</span></li>
-							<li class="list-group-item">Sent: <span class="sent">0</span></li>
-						</ul>
-						<div class="card-body">
-							<h2 class="card-text">Cpu usage</h2>
-							<canvas id="doughnut-cpu"></canvas>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="card" id="cpu">
-						<div class="card-header">
-							CPU's
-						</div>
-						<ul class="list-group list-group-flush"></ul>
-					</div>
-				</div>
-			</div>
-		</div>
+    <body>
+        <div class="container-fluid py-2">
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-warning" role="alert"><b>Note:</b> Content refreshes every <span id="refresh_time"></span> second(s).</div>
+                    <div class="alert alert-info" role="alert" id="general_info">-</div>
+                    <section id="mainchart"></section>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="card" id="ram">
+                        <div class="card-header">
+                            RAM usage
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item list-group-item-danger">Usage: <span class="usage">0</span></li>
+                            <li class="list-group-item list-group-item-info">Total: <span class="total">0</span> GB</li>
+                            <li class="list-group-item list-group-item-success">Free: <span class="free">0</span> GB</li>
+                        </ul>
+                        <div class="card-body">
+                            <canvas id="doughnut-ram"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card" id="hdd">
+                        <div class="card-header">
+                            HDD usage
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item list-group-item-danger">Usage: <span class="usage">0</span> GB</li>
+                            <li class="list-group-item list-group-item-info">Total: <span class="total">0</span> GB</li>
+                            <li class="list-group-item list-group-item-success">Free: <span class="free">0</span> GB</li>
+                        </ul>
+                        <div class="card-body">
+                            <canvas id="doughnut-hdd"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card" id="network">
+                        <div class="card-header">
+                            Network usage
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Received: <span class="rec">0</span></li>
+                            <li class="list-group-item">Sent: <span class="sent">0</span></li>
+                        </ul>
+                        <div class="card-body">
+                            <h2 class="card-text">Cpu usage</h2>
+                            <canvas id="doughnut-cpu"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" id="cpu">
+                        <div class="card-header">
+                            CPU's
+                        </div>
+                        <ul class="list-group list-group-flush"></ul>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.7/js/mdb.min.js"></script>
-		<script src="https://code.highcharts.com/stock/highstock.js"></script>
-		<script type="text/javascript" src="script.js"></script>
-	</body>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.7/js/mdb.min.js"></script>
+        <script src="https://code.highcharts.com/stock/highstock.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+    </body>
 </html>
--- script.js
+++ script.js
@@ -7,169 +7,169 @@ var chart_ram, chart_hdd, chart_cpu;
 
 $(document).ready(function() 
 {
-	$("#refresh_time").text(refresh_time);
-	
-	highChartsInit();
-	
-	chart_ram = createChart("doughnut-ram", ["Free RAM", "Used ram"]);
-	chart_hdd = createChart("doughnut-hdd", ["Free space", "Used space"]);
-	chart_cpu = createChart("doughnut-cpu", ["Unused load", "Cpu load"]);
+    $("#refresh_time").text(refresh_time);
+    
+    highChartsInit();
+    
+    chart_ram = createChart("doughnut-ram", ["Free RAM", "Used ram"]);
+    chart_hdd = createChart("doughnut-hdd", ["Free space", "Used space"]);
+    chart_cpu = createChart("doughnut-cpu", ["Unused load", "Cpu load"]);
 
     setInterval(refresh, refresh_time * 1000); 
 });
 
 function highChartsInit() 
 {
-	Highcharts.setOptions({ global: { useUTC: false } });
-	mainchart = new Highcharts.stockChart({
-		rangeSelector: {
-			buttons: [{
-			count: 1,
-			type: 'minute',
-			text: '1M'
-		},{
-			count: 5,
-			type: 'minute',
-			text: '5M'
-		},{
-			count: 10,
-			type: 'minute',
-			text: '10M'
-		},{
-			count: 15,
-			type: 'minute',
-			text: '15M'
-		},{
-			type: 'all',
-			text: 'All'
-		}],
-			inputEnabled: false,
-			selected: 0
-		},
+    Highcharts.setOptions({ global: { useUTC: false } });
+    mainchart = new Highcharts.stockChart({
+        rangeSelector: {
+            buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        },{
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        },{
+            count: 10,
+            type: 'minute',
+            text: '10M'
+        },{
+            count: 15,
+            type: 'minute',
+            text: '15M'
+        },{
+            type: 'all',
+            text: 'All'
+        }],
+            inputEnabled: false,
+            selected: 0
+        },
 
-		tooltip: { pointFormat: '{series.name}: <b>{point.y}%</b>' },
-		chart: { renderTo: 'mainchart' },
-		title: { text: 'Hardware usage' },
-		xAxis: { title: "Time", type: "datetime" },
+        tooltip: { pointFormat: '{series.name}: <b>{point.y}%</b>' },
+        chart: { renderTo: 'mainchart' },
+        title: { text: 'Hardware usage' },
+        xAxis: { title: "Time", type: "datetime" },
 
-		yAxis: {
-			title: 'Percentage',
-			max: 100,
-			min: 0,
-			tickInterval:10
-		},
+        yAxis: {
+            title: 'Percentage',
+            max: 100,
+            min: 0,
+            tickInterval:10
+        },
 
-		series: [
-			{ name: 'RAM', data: getDummyData() },
-			{ name: 'HDD', data: getDummyData() },
-			{ name: 'CPU', data: getDummyData() }
-		]
+        series: [
+            { name: 'RAM', data: getDummyData() },
+            { name: 'HDD', data: getDummyData() },
+            { name: 'CPU', data: getDummyData() }
+        ]
     });
 }
 
 function createChart(forid, labelarray) {
-	return new Chart(document.getElementById(forid).getContext('2d'), {
-		type: 'doughnut',
-		data: {
-			labels: labelarray,
-			datasets: [{
-				data: [0, 0],
-				backgroundColor: ["#c3e6cb", "#f5c6cb" ],
-				hoverBackgroundColor: ["#155724", "#721c24"]
-			}]
-		},
-		options: {
-			responsive: true,
-			legend: {
-				display: false
-			}
-		}
-	});
+    return new Chart(document.getElementById(forid).getContext('2d'), {
+        type: 'doughnut',
+        data: {
+            labels: labelarray,
+            datasets: [{
+                data: [0, 0],
+                backgroundColor: ["#c3e6cb", "#f5c6cb" ],
+                hoverBackgroundColor: ["#155724", "#721c24"]
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            }
+        }
+    });
 }
 
 function getDummyData()
 {
-	var chartdata = new Array(), curtime = new Date().getTime();
+    var chartdata = new Array(), curtime = new Date().getTime();
 
-	for (i = -399; i <= 0; i++)	chartdata.push([curtime + i * 1000, 0]);
-		chartdata.push([curtime, 0]);
+    for (i = -399; i <= 0; i++)	chartdata.push([curtime + i * 1000, 0]);
+        chartdata.push([curtime, 0]);
 
-	return chartdata;
+    return chartdata;
 }
 
 function refresh()
 {
-	$.getJSON("getData.php", null, function(data) {
-		var time = (new Date()).getTime();
+    $.getJSON("getData.php", null, function(data) {
+        var time = (new Date()).getTime();
 
-		var cpuload = getCpuLoad(data.CPUDetail);
-		var currentram = ((data.memory[1] / data.memory[0]) * 100).toFixed(2);
-		var currenthdd = ((data.storage["used"] / data.storage["total"]) * 100).toFixed(2);
-		var currentcpu = cpuload > 100 ? 100 : cpuload;
+        var cpuload = getCpuLoad(data.CPUDetail);
+        var currentram = ((data.memory[1] / data.memory[0]) * 100).toFixed(2);
+        var currenthdd = ((data.storage["used"] / data.storage["total"]) * 100).toFixed(2);
+        var currentcpu = cpuload > 100 ? 100 : cpuload;
 
-		mainchart.series[0].addPoint([time, parseFloat(currentram)], false, true);
-		mainchart.series[1].addPoint([time, parseFloat(currenthdd)], false, true);
-		mainchart.series[2].addPoint([time, parseFloat(currentcpu)], true, true);
+        mainchart.series[0].addPoint([time, parseFloat(currentram)], false, true);
+        mainchart.series[1].addPoint([time, parseFloat(currenthdd)], false, true);
+        mainchart.series[2].addPoint([time, parseFloat(currentcpu)], true, true);
 
-		$("#ram .usage").html(formatNumber(data.memory[1]) + " GB<br/>Cache: " + formatNumber(data.memory[3]) + " GB");
-		$("#ram .total").text(formatNumber(data.memory[0]));
-		$("#ram .free").text(formatNumber(data.memory[2]));
+        $("#ram .usage").html(formatNumber(data.memory[1]) + " GB<br/>Cache: " + formatNumber(data.memory[3]) + " GB");
+        $("#ram .total").text(formatNumber(data.memory[0]));
+        $("#ram .free").text(formatNumber(data.memory[2]));
 
-		$("#hdd .usage").text(formatNumber(data.storage["used"]));
-		$("#hdd .total").text(formatNumber(data.storage["total"]));
-		$("#hdd .free").text(formatNumber(data.storage["free"]));
+        $("#hdd .usage").text(formatNumber(data.storage["used"]));
+        $("#hdd .total").text(formatNumber(data.storage["total"]));
+        $("#hdd .free").text(formatNumber(data.storage["free"]));
 
-		$("#network .rec").html(formatNumber(data.network[0]) + " bytes <br/>Packets: " + formatNumber(data.network[1]));
-		$("#network .sent").html(formatNumber(data.network[2]) + " bytes <br/>Packets: " + formatNumber(data.network[3]));
+        $("#network .rec").html(formatNumber(data.network[0]) + " bytes <br/>Packets: " + formatNumber(data.network[1]));
+        $("#network .sent").html(formatNumber(data.network[2]) + " bytes <br/>Packets: " + formatNumber(data.network[3]));
 
-		var info = "Uptime: " + getTime(data.uptime) + "<br />Operating System: " + data.OS;
-		$("#general_info").html(info);
+        var info = "Uptime: " + getTime(data.uptime) + "<br />Operating System: " + data.OS;
+        $("#general_info").html(info);
 
-		$("#cpu .list-group").empty();
+        $("#cpu .list-group").empty();
 
-		chart_ram.data.datasets[0].data = [data.memory[2], data.memory[1]];
-		chart_hdd.data.datasets[0].data = [data.storage["free"], data.storage["used"]];
-		chart_cpu.data.datasets[0].data = [100.0 - currentcpu, currentcpu];
-		chart_ram.update();
-		chart_hdd.update();
-		chart_cpu.update();
+        chart_ram.data.datasets[0].data = [data.memory[2], data.memory[1]];
+        chart_hdd.data.datasets[0].data = [data.storage["free"], data.storage["used"]];
+        chart_cpu.data.datasets[0].data = [100.0 - currentcpu, currentcpu];
+        chart_ram.update();
+        chart_hdd.update();
+        chart_cpu.update();
 
-		for (var i = 0; i < data.CPU.length; i++) 
-		{
-			if(i % 3 === 0)
-			{
-				var listitem = $("<li>", {"class": "list-group-item"});
-				listitem.html(data.CPU[i][1] + "<br />" + data.CPU[i + 1][0] + ": " + data.CPU[i + 1][1] + "<br />" + data.CPU[i + 2][0] + ": " + data.CPU[i + 2][1]);
-				$("#cpu .list-group").append(listitem);
-			}
-		}
-	});
+        for (var i = 0; i < data.CPU.length; i++) 
+        {
+            if(i % 3 === 0)
+            {
+                var listitem = $("<li>", {"class": "list-group-item"});
+                listitem.html(data.CPU[i][1] + "<br />" + data.CPU[i + 1][0] + ": " + data.CPU[i + 1][1] + "<br />" + data.CPU[i + 2][0] + ": " + data.CPU[i + 2][1]);
+                $("#cpu .list-group").append(listitem);
+            }
+        }
+    });
 }
 
 //Calculation by https://github.com/Leo-G/DevopsWiki/wiki/How-Linux-CPU-Usage-Time-and-Percentage-is-calculated
 function getCpuLoad(input)
 {
-	var cpuload = input.split(' ');
-	var sum = 0;
+    var cpuload = input.split(' ');
+    var sum = 0;
 
-	for (var i = 0; i < cpuload.length; i++) {
-		sum += parseInt(cpuload[i]);	
-	}
+    for (var i = 0; i < cpuload.length; i++) {
+        sum += parseInt(cpuload[i]);	
+    }
 
-	var idlecpuload = cpuload[3];
-	var diff_idle = idlecpuload - prev_idle;
-	var diff_total = sum - prev_total;
-	var diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10;
+    var idlecpuload = cpuload[3];
+    var diff_idle = idlecpuload - prev_idle;
+    var diff_total = sum - prev_total;
+    var diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10;
 
-	prev_total = sum;
-	prev_idle = idlecpuload;
-	
-	return diff_usage.toFixed(2);
+    prev_total = sum;
+    prev_idle = idlecpuload;
+    
+    return diff_usage.toFixed(2);
 }
 
 function formatNumber(number)
 {
-	return number.toLocaleString("nl").replace(/\./g, " ");
+    return number.toLocaleString("nl").replace(/\./g, " ");
 }
 
 function getTime(seconds) 
